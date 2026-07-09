d12254a0d8bbf5fe540ce558df67a2c815ced2ba
Abheeshta-P <softwarecheck01+github@gmail.com>
2026-07-09 15:40:43 +0530

    chore(prod): add docker and nginx configuration for production

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:8080";
+  import.meta.env.VITE_SERVER_URL !== undefined ? import.meta.env.VITE_SERVER_URL : "http://localhost:8080";
 
 export const API_ENDPOINTS = {
   CALCULATE_ROUTE: `${SERVER_URL}/api/route/calculate`,
